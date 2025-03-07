d4be59c7db4782fce9ae10744f445ecf26dba850
Helio9284 <colonelme9284@gmail.com>
2025-03-06 21:33:34 -0500

    api bug fix

--- src/web3/Login/Login.js
+++ src/web3/Login/Login.js
@@ -15,21 +15,6 @@ const INFURA_ID = process.env.INFURA_API_KEY;
 const providerOptions = {
   walletconnect: {
     package: WalletConnectProvider, // required
-    options: {
-      infuraId: INFURA_ID, // required
-    },
-  },
-  'custom-walletlink': {
-    display: {
-       logo: 'https://play-lh.googleusercontent.com/PjoJoG27miSglVBXoXrxBSLveV6e3EeBPpNY55aiUUBM9Q1RCETKCOqdOkX2ZydqVf0',
-      name: 'Coinbase',
-      description: 'Connect to Coinbase Wallet (not Coinbase App)'
-    },
-    options: {
-      appName: 'Coinbase', // Your app name
-      networkUrl: `https://mainnet.infura.io/v3/${INFURA_ID}`,
-      chainId: 1
-    },
     package: WalletLink,
     connector: async (_, options) => {
       const { appName, networkUrl, chainId } = options
